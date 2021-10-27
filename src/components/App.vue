0fc0846fd070e177060bd2d2250e11bd73a3515f
Sandra Sügav <MM-20-337@tptlive.ee>
2021-10-27 11:33:20 +0300

    ffff

--- src/components/App.vue
+++ src/components/App.vue
@@ -20,17 +20,19 @@
 <script>
 import ApiExample from './ApiExample.vue'
 import ClickerExample from './ClickerExample.vue'
+import InfiniteScrollExample from './InfiniteScrollExample.vue'
 import ModalExample from './ModalExample.vue'
 import RickAndMortyExample from './RickAndMortyExample.vue'
 import ToDoExample from './ToDoExample.vue'
 
 
 export default {
-  components: { ModalExample, ToDoExample, ClickerExample, ApiExample, RickAndMortyExample},
+  components: { ModalExample, ToDoExample, ClickerExample, ApiExample, RickAndMortyExample, InfiniteScrollExample},
     data(){
       return {
         active: 0,
         items: [
+          {title:'Infinite Scroll Example', componentName:'InfiniteScrollExample'},
           {title:'Rick and Morty Example', componentName:'RickAndMortyExample'},
           {title:'Api Example', componentName:'ApiExample'},
           {title:'Clicker Example', componentName:'ClickerExample'},
---
+++ src/components/InfiniteScrollExample.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+    <div class="columns is-multiline" @scroll="scroll">
+        <div class="column is-one-quarter" v-for="result in results" :key="result.id">
+            <character-card :character="result"></character-card>
+        </div>
+    </div>
+    <h1 class="is-size-1" v-if="!info.next">There are no results</h1>
+</div>
+</template>
+
+<script>
+import CharacterCard from './CharacterCard.vue';
+import Pagination from './Pagination.vue';
+const axios = require("axios")
+export default {
+  components: { CharacterCard, Pagination },
+    created(){
+        axios.get("https://rickandmortyapi.com/api/character").then(response => {
+            console.log(response.data);
+            this.info = response.data.info;
+            this.results = response.data.results;
+        });
+        window.addEventListener("scroll",this.scroll)
+    },
+    data(){
+        return {
+            info: {
+                next: null,
+                prev: null,
+                pages: 0,
+                count: 0 
+            },
+            results: [],
+            current: 1
+        }
+    },
+    methods: {
+        next(){
+            axios.get(this.info.next).then(response => {
+            console.log(response.data);
+            this.info = response.data.info;
+            this.results.push(...response.data.results);
+            }); 
+        },
+        scroll(){
+            console.log(window.scrollY + window.innerHeight, document.body.clientHeight);
+            if(window.scrollY + window.innerHeight == document.body.clientHeight && this.info.next){
+                this.next();
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
