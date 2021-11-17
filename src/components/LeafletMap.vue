3cdeabe48c331f1c0367e5840fcea0f97bbfba1c
Sandra Sügav <MM-20-337@tptlive.ee>
2021-11-17 11:09:53 +0200

    gg

--- src/components/LeafletMap.vue
+++ src/components/LeafletMap.vue
@@ -6,7 +6,7 @@
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 export default {
-    props: ['lat', 'lng', 'zoom'],
+    props: ['lat', 'lng', 'zoom', 'geoJson'],
     mounted(){
         this.map = L.map('map').setView([this.lat, this.lng], this.zoom);
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -17,10 +17,25 @@ export default {
             zoomOffset: -1,
             accessToken: 'pk.eyJ1IjoicHV1bWV0YWxsIiwiYSI6ImNrdzM3bnNtbTBzM3Yybm1veHlvbXk2NnAifQ.jf7i9VdMGoPlR0VZ6rFGug'
         }).addTo(this.map);
+         L.geoJson(this.geoJson).addTo(this.map);
     },
     data(){
         return {
             map: null
+        }
+    },
+   watch: {
+        lat(lat){
+            this.map.flyTo([lat, this.lng], this.zoom);
+        },
+        lng(lng){
+            this.map.flyTo([this.lat, lng], this.zoom);
+        },
+        zoom(zoom){
+            this.map.flyTo([this.lat, this.lng], zoom);
+        },
+        geoJson(geoJson){
+            L.geoJson(this.geoJson).addTo(this.map);
         }
     }
 }
--- src/components/MapExample.vue
+++ src/components/MapExample.vue
@@ -3,32 +3,65 @@
     <button class="button is-primary" @click="goToHome">Go to home</button>
     <button class="button is-danger" @click="goToSchool">Go to school</button>
     <!-- <google-maps :lat="this.lat" :lng="this.lng" :zoom="this.zoom"></google-maps> -->
-    <leaflet-map :lat="this.lat" :lng="this.lng" :zoom="this.zoom"></leaflet-map>
+    <leaflet-map :lat="lat" :lng="lng" :zoom="zoom" :geoJson="geoJson"></leaflet-map>
 </div>
 </template>
 
 <script>
-import GoogleMaps from './GoogleMaps.vue'
-import LeafletMap from './LeafletMap.vue'
+import GoogleMaps from './GoogleMaps.vue';
+import LeafletMap from './LeafletMap.vue';
+const axios= require('axios');
 export default {
     components: { GoogleMaps, LeafletMap },
+    created(){
+        let url = 'https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json'
+        axios.get(url).then(response =>{
+            console.log(response.data);
+            this.geoJson = response.data
+        });
+        axios.get('https://api.covid19api.com/summary').then(response => {
+            console.log(response);
+            this.countries = response.data.Countries;
+        })
+    },
     data(){
         return {
             lat:59.4267818,
             lng:24.7435557,
-            zoom:18.25
+            zoom:18.25,
+            geoJson: null,
+            countries: null
         }
     },
     methods: {
         goToHome(){
             this.lat=59.4116718,
             this.lng=24.704622,
-            this.zoom=18.25
+            this.zoom=17
         },
         goToSchool(){
             this.lat=59.4268366,
             this.lng=24.7430622,
-            this.zoom=18.25
+            this.zoom=17
+        }
+    },
+    computed: {
+        covidGeoJson() {
+            if(this.geoJson && this.countries) {
+                let covidGeoJson = {...this.geoJson};
+                covidGeoJson.features = this.geoJson.features.map(feature => {
+                    let country = this.countries.find(country => country.Country == feature.properties.name);
+                    feature.properties.confirmed = 0;
+                    feature.properties.deaths = 0;
+                    if(country){
+                        feature.properties.confirmed = country.TotalConfirmed;
+                        feature.properties.deaths = country.TotalDeaths;
+                    }
+                    return feature;
+                });
+                return covidGeoJson;
+            }
+            return null;
         }
     }
 }
